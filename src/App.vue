4ec0c5a2b04408b19eed754d901d94aeaeb56012
lesya-val <you@example.com>
2023-07-28 16:37:04 +0600

    feat: create main page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div></div>
+	<MainPage/>
 </template>
 
 <script>
 
+import MainPage from '@/pages/MainPage.vue'
+
 export default {
   name: 'App',
   components: {
-    
+    MainPage,
   }
 }
 </script>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="main-page">
+		
+	</div>
+</template>
+
+<script>
+
+
+
+export default {
+	name: 'App',
+	components: {
+		
+	}
+}
+</script>
+
+<style>
+
+</style>
